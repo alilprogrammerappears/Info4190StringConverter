15e12b06e7ec3082abc6ea7adfce18a8c22840b5
Kassandra <montgomery135@hotmail.com>
2024-04-03 18:09:53 -0700

    updated uppercase, reorganized, and added test

--- ConvertUpper.java
+++ ConvertUpper.java
@@ -1,8 +1,9 @@
 public class ConvertUpper {
     
-    public void convertUppercase(String stringInput) {
+    public String convertUppercase(String stringInput) {
         String convertedString = stringInput.toUpperCase();
-        System.out.println("Uppercase string: " + convertedString);
+        
+        return convertedString;
     }
 
 
